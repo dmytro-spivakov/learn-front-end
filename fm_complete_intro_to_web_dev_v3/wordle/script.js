e23aa6d1a74088427be6e96b1e03bd2a52e9202b
Dmytro Spivakov <dmytro_spivakov@proton.me>
2025-03-02 05:03:18 +0200

    wordle: done

--- fm_complete_intro_to_web_dev_v3/wordle/script.js
+++ fm_complete_intro_to_web_dev_v3/wordle/script.js
@@ -1,9 +1,14 @@
+const WORD_FETCH_URL = "https://words.dev-apis.com/word-of-the-day"
+const WORD_VALIDATE_URL = "https://words.dev-apis.com/validate-word"
+
+let wordOfTheDay;
+
 let currentRowIdx = 0;
 let currentCellIdx = 0;
 
 let rows = document.querySelectorAll('.game-row');
 
-function handleKeystroke(event) {
+async function handleKeystroke(event) {
 	console.log(event);
 	console.log(`${event.key} is a valid key - ${isValidKey(event.key)}`);
 
@@ -22,7 +27,13 @@ function handleKeystroke(event) {
 			alert("You lost!");
 		}
 
-		if (currentCellIdx > 4) {
+		const commitRes = await commitRow();
+		if (currentCellIdx > 4 && commitRes) {
+			const cells = rowByIdx(currentRowIdx).querySelectorAll(".game-cell.correct-cell");
+			if (cells.length >= 5) {
+				alert("You won!");
+			}
+
 			currentRowIdx += 1;
 			currentCellIdx = 0;
 		} else {
@@ -86,7 +97,93 @@ function isEnd() {
 	return currentRowIdx >= 5 && currentCellIdx > 4
 }
 
+async function commitRow() {
+	const currentRowCells = rowByIdx(currentRowIdx).querySelectorAll(".game-cell");
+	let currentWord = "";
+	currentRowCells
+		.forEach((cell) => { currentWord += cell.innerHTML });
+
+	if (!await validateWord(currentWord)) {
+		flashErrorOnCurrentRow();
+		return false;
+	}
+
+	let wodChars = wordOfTheDay.split("");
+	let charCounter = wodChars.reduce((obj, char) => {
+		if (obj[char] == undefined) {
+			obj[char] = 0;
+		}
+		obj[char] += 1;
+
+		return obj;
+	}, {});
+	currentRowCells.forEach((cell, currIdx) => {
+		cell.classList.add("committed-cell");
+
+		const currChar = cell.innerHTML.toUpperCase();
+		const idx = wodChars.indexOf(currChar);
+		if (idx < 0) {
+			cell.classList.add("wrong-cell");
+		} else if (idx === currIdx) {
+			cell.classList.add("correct-cell");
+			charCounter[currChar] -= 1;
+		} else {
+			charCounter[currChar] -= 1;
+			if (charCounter[currChar] < 0) {
+				cell.classList.add("wrong-cell");
+			} else {
+				cell.classList.add("mismatched-cell");
+			}
+		}
+	});
+	return true;
+}
+
+async function validateWord(word) {
+	try {
+		const response = await fetch(WORD_VALIDATE_URL, { method: "POST", body: JSON.stringify({ word: word }) });
+
+		if (!response.ok) {
+			alert(`validate word unexpected response - ${response}`);
+		}
+
+		const data = await response.json();
+		if (typeof data.validWord !== "boolean") {
+			alert(`validate word unexpected payload - ${data}`);
+		}
+
+		return data.validWord;
+	} catch (error) {
+		alert(`validate word error - ${error}`);
+		throw error;
+	}
+}
+
+async function fetchWordOfTheDay() {
+	try {
+		const response = await fetch(WORD_FETCH_URL, { method: "GET" });
+
+		if (!response.ok) {
+			alert(`Word fetch API request failed with - ${response}`);
+		}
+
+		const data = await response.json();
+		if (typeof data.word !== "string" || data.word.length != 5) {
+			alert(`Unexpected word value, response - ${data}`);
+		}
+		return data.word;
+	} catch (error) {
+		alert(`Word fetch failed with - ${error}`);
+		throw error;
+	}
+
+}
+
 function init() {
+	fetchWordOfTheDay()
+		.then(data => {
+			wordOfTheDay = data.toUpperCase();
+		});
 	document.addEventListener("keyup", handleKeystroke);
 }
 
